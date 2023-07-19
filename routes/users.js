7421fa413c7989e4573c2ad7ca10e059093db4cd
burondt <denisalexandrovich@inbox.ru>
2023-07-19 21:39:47 +0300

    commit

---
+++ routes/users.js
@@ -0,0 +1,10 @@
+//users routers
+const router = require('express').Router();
+const { validationUpdateUser } = require('../middlewares/validation');
+
+const { updateProfile, getMe } = require('../controllers/users');
+
+router.get('/me', getMe);
+router.patch('/me', validationUpdateUser, updateProfile);
+
+module.exports = router;
